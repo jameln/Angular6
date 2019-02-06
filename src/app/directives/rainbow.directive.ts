ee9a38aead893bf73c7955ca116298583e4f22b4
jameln <djameloter@gmail.com>
2019-02-06 14:35:04 +0100

    directive

--- src/app/directives/rainbow.directive.ts
+++ src/app/directives/rainbow.directive.ts
@@ -6,12 +6,19 @@ import { Directive, Input, HostListener, HostBinding } from '@angular/core';
 export class RainbowDirective {
   @Input() borderColor = 'red';
   @Input() textColor = 'green';
-  tableColor = ['red','gray','green','blue','yellow','black'];
+  tableColor = [
+    'red',
+    'gray',
+    'green',
+    'blue',
+    'yellow',
+    'black'
+    ];
   @HostBinding('style.color') color = this.textColor;
   @HostBinding('style.border-color') bordor = this.borderColor;
   constructor() { }
   @HostListener('keypress') changeColor() {
-    this.color = this.tableColor[Math.floor(Math.random()*this.tableColor.length-1)];
-    this.bordor = this.tableColor[Math.floor(Math.random()*this.tableColor.length-1)];
+    this.color = this.tableColor[Math.floor(Math.random() * (this.tableColor.length - 1))];
+    this.bordor = this.tableColor[Math.floor(Math.random() * (this.tableColor.length - 1))];
   }
 }
